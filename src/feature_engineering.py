3da153a4348d106f65d683d55fc5867f843da457
RahimAbbas55 <irahimsindhu@gmail.com>
2026-07-23 00:21:30 +0500

    Day 2: Added a feature engineering function to cater the DAYS_EMPLOYED anomaly

---
+++ src/feature_engineering.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def fix_days_employed_anomaly(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Fix the DAYS_EMPLOYED anomaly where 365243 is used as a placeholder
+    (likely for retired/unemployed applicants).
+    Creates a flag column before replacing the placeholder with NaN.
+    '''
+    df = df.copy()
+    ANOMALY_VALUE = 356243
+    df['DAYS_EMPLOYED_ANM'] = df['DAYS_EMPLOYED'] == ANOMALY_VALUE
+    df['DAYS_EMPLOYED'] = df['DAYS_EMPLOYED'].replace(ANOMALY_VALUE , pd.NA)
+    return df
